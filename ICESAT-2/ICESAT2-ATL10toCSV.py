e656b1ff0930d805356db808b85f759e1eee89c3
John Baker <bkrjuan68@gmail.com>
2023-05-04 00:00:09 -0700

    Committed support for all 3 strong beams + along-ground track change

--- ICESAT-2/ICESAT2-ATL10toCSV.py
+++ ICESAT-2/ICESAT2-ATL10toCSV.py
@@ -28,6 +28,10 @@ if(len(sys.argv) >= 1):
             quit()
         OUT_FILE = FILE_NAME[:-3] + ".csv"
 
+
+        # Consider adding a for loop to do this same process but for all 6 beams gt1r/l, gt2r/l, gt3r/l
+        # Ex: Declare the starting beam names, and iterate through that array and push the variables into another array
+        #       where you then supply them into df
         with h5py.File(FILE_NAME, mode='r') as f:
 
             latvar = f['/gt1r/freeboard_beam_segment/beam_freeboard/latitude']
@@ -38,20 +42,54 @@ if(len(sys.argv) >= 1):
             longitude = lonvar[:]
             longitude = list(longitude)
             
+            # Start reading in the first set of GT1R beams
             dset_name = '/gt1r/freeboard_beam_segment/beam_freeboard/beam_fb_height'
             datavar = f[dset_name]
             data = datavar[:]
 
-            units = datavar.attrs['units']
-            long_name = datavar.attrs['long_name']
+            # # Assign the Fill Value once
             _FillValue = datavar.attrs['_FillValue']
-            # Handle FillValue
+
+            # # Handle FillValue
             data[data == _FillValue] = np.nan
             data = np.ma.masked_where(np.isnan(data), data)
+
+            # Read in the second (central) strong beam
+            dset_name = '/gt2r/freeboard_beam_segment/beam_freeboard/beam_fb_height'
+            datavar = f[dset_name]
+            data2 = datavar[:]
+
+            # Handle FillValue
+            data2[data2 == _FillValue] = np.nan
+            data2 = np.ma.masked_where(np.isnan(data2), data2)
+
+            # Read in the last (central) strong beam
+            dset_name = '/gt3r/freeboard_beam_segment/beam_freeboard/beam_fb_height'
+            datavar = f[dset_name]
+            data3 = datavar[:]
+
+            # Handle FillValue
+            data3[data3 == _FillValue] = np.nan
+            data3 = np.ma.masked_where(np.isnan(data3), data3)
 
             timevar = f['/gt1r/freeboard_beam_segment/beam_freeboard/delta_time']
             time = timevar[:]
             time = list(time)
+
+            data2conf = f['/gt2r/freeboard_beam_segment/beam_freeboard/beam_fb_confidence']
+            data2conf = list(data2conf[:])
+
+            heightSegConf = f['/gt2r/freeboard_beam_segment/height_segments/height_segment_confidence']
+            heightSegConf = list(heightSegConf[:])
+
+            heightSegType = f['/gt2r/freeboard_beam_segment/height_segments/height_segment_type']
+            heightSegType = list(heightSegType[:])
+
+            heightSegRefl = f['/gt2r/freeboard_beam_segment/height_segments/asr_25']
+            heightSegRefl = list(heightSegRefl[:])
+
+            qualityFlag = f["/gt2r/freeboard_beam_segment/beam_freeboard/beam_fb_quality_flag"]
+            qualityFlag = list(qualityFlag[:])
 
             dateTime = []
             GPS_EPOCH = datetime.datetime(2018,1,1).timestamp()    # Passing in the ATLAS EPOCH
@@ -59,33 +97,53 @@ if(len(sys.argv) >= 1):
             for timeInSec in time:
                 calendarTime = datetime.datetime.fromtimestamp(timeInSec+GPS_EPOCH).strftime('\"%H:%M:%S.%f %Y-%m-%d\"')
                 dateTime.append(calendarTime)
-
-            df = pd.DataFrame({
-                "Latitude": latitude,
-                "Longitude": longitude,
-                "Freeboard Height": data,
-                "Time (seconds since 1/1/2018)": time,
-                "Calendar Time": dateTime
-            })
-            df = df.replace(r'^\s*$', np.nan, regex=True)
-            df = df.dropna()
-
-            validLatitudes = list(df['Latitude'])
-            validLongitudes = list(df['Longitude'])
-
+            
             distance = []
-            x1 = validLongitudes[0]
-            y1 = validLatitudes[0]
+            x1 = longitude[0]
+            y1 = latitude[0]
 
-            for i in range(len(validLatitudes)):
-                x2 = validLongitudes[i]
-                y2 = validLatitudes[i]
+            for i in range(len(latitude)):
+                x2 = longitude[i]
+                y2 = latitude[i]
                 deltaDistance = hs.haversine((y1,x1), (y2, x2), normalize=True, unit="m")
                 distance.append(deltaDistance)
                 x1= x2
                 y1 = y2
 
-            df.insert(3, "Along-Ground Distance Change (m)",distance)
+            print("Freeboards & location")
+            print(len(latitude))
+            print(len(longitude))
+            print(len(data))
+            print(len(data2))
+            print(len(data3))
+            print("Heights / confidence:")
+            print(len(data2conf))
+            print(len(heightSegConf))
+            print(len(heightSegType))
+            print(len(heightSegRefl))
+            print("Time")
+            print(len(time))
+            print(len(dateTime))
+
+            df_dict = {
+                "Latitude": latitude,
+                "Longitude": longitude,
+                "1FBH": data,
+                "2FBH": data2,
+                "3FBH": data3,
+                "2FBCONF": data2conf,
+                "2HSCONF": heightSegConf,
+                "2HSTYPE": heightSegType,
+                "2HSREFL": heightSegRefl,
+                "Quality": qualityFlag,
+                "GroundDist": distance,
+                "Time (seconds since 1/1/2018)": time,
+                "Calendar Time": dateTime
+            }
+
+            df = pd.DataFrame(dict([(k,pd.Series(v)) for k,v in df_dict.items()]))
+            df = df.replace(r'^\s*$', np.nan, regex=True)
+            # df = df.dropna()
 
             df.to_csv(OUT_FILE, date_format=None)
 
