a498e4303e6515526020ab27b4d8edfd40605a5a
John Baker <bkrjuan68@gmail.com>
2023-09-06 15:44:32 -0700

    Updated script to pull data from all 6 beams

--- ICESAT-2/ICESAT2-ATL10toCSV.py
+++ ICESAT-2/ICESAT2-ATL10toCSV.py
@@ -34,124 +34,127 @@ if(len(sys.argv) >= 1):
         #       where you then supply them into df
         with h5py.File(FILE_NAME, mode='r') as file:
 
-            filePathToBeamData = "freeboard_beam_segment/beam_freeboard"
-            filePathToHeightData = "freeboard_beam_segment/height_segments"
+            # We'll need to restructure our script to call functions that pull this information out.
+            # We could make a function and pass in these variables as keys? That way we can avoid drawing them out.
+            beam_enum = ['gt1r', 'gt1l', 'gt2r', 'gt2l', 'gt3r', 'gt3l']
+            data_cols = []
 
-            try:
-                latvar = file[f'/gt1r/{filePathToBeamData}/latitude']
-            except:
+            df_dict = {}
+            # gt1rLat, gt1rLon, gt1lLat, gt1lLon,
+            # gt2rLat, gt2rLon, gt2lLat, gt2lLon,
+            # gt3rLat, gt3rLon, gt3lLat, gt3lLon
+
+            if "gt1r/freeboard_beam_segment/beam_freeboard" in file:
+                filePathToBeamData = "freeboard_beam_segment/beam_freeboard"
+            else:
                 filePathToBeamData = "freeboard_segment"
+
+            if "gt1r/freeboard_beam_segment/height_segments" in file:
+                filePathToHeightData = "freeboard_beam_segment/height_segments"
+            else:
                 filePathToHeightData = "freeboard_segment/heights"
-                latvar = file[f'/gt1r/{filePathToBeamData}/latitude']
-            latitude = latvar[:]
-            latitude = list(latitude)
-            
-            lonvar = file[f'/gt1r/{filePathToBeamData}/longitude']
-            longitude = lonvar[:]
-            longitude = list(longitude)
-            
-            # Start reading in the first set of GT1R beams
-            dset_name = f'/gt1r/{filePathToBeamData}/beam_fb_height'
-            datavar = file[dset_name]
-            data = datavar[:]
 
-            # # Assign the Fill Value once
-            _FillValue = datavar.attrs['_FillValue']
+            print(filePathToBeamData)
+            print(filePathToHeightData)
 
-            # # Handle FillValue
-            data[data == _FillValue] = np.nan
-            data = np.ma.masked_where(np.isnan(data), data)
+            for beam in beam_enum:
+                print(beam)
+                # Read in the lat/lon
+                latvar = file[f'/{beam}/{filePathToBeamData}/latitude']
+                latitude = list(latvar[:])
+                lonvar = file[f'/{beam}/{filePathToBeamData}/longitude']
+                longitude = list(lonvar[:])
 
-            # Read in the second (central) strong beam
-            dset_name = f'/gt2r/{filePathToBeamData}/beam_fb_height'
-            datavar = file[dset_name]
-            data2 = datavar[:]
+                # Read in the freeboard height
+                dset_name = f'/{beam}/{filePathToBeamData}/beam_fb_height'
+                datavar = file[dset_name]
+                data = datavar[:]
 
-            # Handle FillValue
-            data2[data2 == _FillValue] = np.nan
-            data2 = np.ma.masked_where(np.isnan(data2), data2)
+                # Assign the Fill Value once
+                _FillValue = datavar.attrs['_FillValue']
 
-            # Read in the last (central) strong beam
-            dset_name = f'/gt3r/{filePathToBeamData}/beam_fb_height'
-            datavar = file[dset_name]
-            data3 = datavar[:]
+                # Handle FillValue
+                data[data == _FillValue] = np.nan
+                data = np.ma.masked_where(np.isnan(data), data)
 
-            # Handle FillValue
-            data3[data3 == _FillValue] = np.nan
-            data3 = np.ma.masked_where(np.isnan(data3), data3)
+                # Read in the time and convert to readable
+                timevar = file[f'/{beam}/{filePathToBeamData}/delta_time']
+                time = list(timevar[:])
+                dateTime = []
+                GPS_EPOCH = datetime.datetime(2018,1,1).timestamp()    # Passing in the ATLAS EPOCH
 
-            timevar = file[f'/gt1r/{filePathToBeamData}/delta_time']
-            time = timevar[:]
-            time = list(time)
+                for timeInSec in time:
+                    calendarTime = datetime.datetime.fromtimestamp(timeInSec+GPS_EPOCH).strftime('\"%H:%M:%S.%f %Y-%m-%d\"')
+                    dateTime.append(calendarTime)
 
-            data2conf = file[f'/gt2r/{filePathToBeamData}/beam_fb_confidence']
-            data2conf = list(data2conf[:])
+                # Read in the beam confidence
+                dataConf = file[f'/{beam}/{filePathToBeamData}/beam_fb_confidence']
+                dataConf = list(dataConf[:])
 
-            heightSegConf = file[f'/gt2r/{filePathToHeightData}/height_segment_confidence']
-            heightSegConf = list(heightSegConf[:])
+                # Read in the height confidence
+                heightSegConf = file[f'/{beam}/{filePathToHeightData}/height_segment_confidence']
+                heightSegConf = list(heightSegConf[:])
 
-            heightSegType = file[f'/gt2r/{filePathToHeightData}/height_segment_type']
-            heightSegType = list(heightSegType[:])
+                # Read in the height segment type
+                heightSegType = file[f'/{beam}/{filePathToHeightData}/height_segment_type']
+                heightSegType = list(heightSegType[:])
 
-            heightSegRefl = file[f'/gt2r/{filePathToHeightData}/asr_25']
-            heightSegRefl = list(heightSegRefl[:])
+                # Read in the reflectance
+                heightSegRefl = file[f'/{beam}/{filePathToHeightData}/asr_25']
+                heightSegRefl = list(heightSegRefl[:])
 
-            qualityFlag = file[f"/gt2r/{filePathToBeamData}/beam_fb_quality_flag"]
-            qualityFlag = list(qualityFlag[:])
+                # Read in the quality flag
+                qualityFlag = file[f"/{beam}/{filePathToBeamData}/beam_fb_quality_flag"]
+                qualityFlag = list(qualityFlag[:])
+                
+                # Calculate ground distance
+                distance = []
+                x1 = longitude[0]
+                y1 = latitude[0]
 
-            dateTime = []
-            GPS_EPOCH = datetime.datetime(2018,1,1).timestamp()    # Passing in the ATLAS EPOCH
+                for i in range(len(latitude)):
+                    x2 = longitude[i]
+                    y2 = latitude[i]
+                    deltaDistance = hs.haversine((y1,x1), (y2, x2), normalize=True, unit="m")
+                    distance.append(deltaDistance)
+                    x1= x2
+                    y1 = y2
 
-            for timeInSec in time:
-                calendarTime = datetime.datetime.fromtimestamp(timeInSec+GPS_EPOCH).strftime('\"%H:%M:%S.%f %Y-%m-%d\"')
-                dateTime.append(calendarTime)
-            
-            distance = []
-            x1 = longitude[0]
-            y1 = latitude[0]
+                print("Beam: ", beam)
+                print("Freeboards & location lengths")
+                print(len(latitude))
+                print(len(longitude))
+                print(len(data))
+                print("Heights / confidence lengths:")
+                print(len(dataConf))
+                print(len(heightSegConf))
+                print(len(heightSegType))
+                print(len(heightSegRefl))
+                print("Time")
+                print(len(time))
+                print(len(dateTime))
+                
+                # We want to append the lat/lon, fbheights, and ground distance for each beam, and then dateTime only on the last
+                latCol = beam + "Lat"
+                lonCol = beam + "Lon"
+                fbCol = beam + "FBH"
+                distCol = beam + "Dist(m)"
+                df_dict[latCol] = latitude
+                df_dict[lonCol] = longitude
+                df_dict[fbCol] = data
+                df_dict[distCol] = distance
 
-            for i in range(len(latitude)):
-                x2 = longitude[i]
-                y2 = latitude[i]
-                deltaDistance = hs.haversine((y1,x1), (y2, x2), normalize=True, unit="m")
-                distance.append(deltaDistance)
-                x1= x2
-                y1 = y2
+                # Keep track of the data columns so we can remove nan values
+                data_cols.append(fbCol)
+                
 
-            print("Freeboards & location")
-            print(len(latitude))
-            print(len(longitude))
-            print(len(data))
-            print(len(data2))
-            print(len(data3))
-            print("Heights / confidence:")
-            print(len(data2conf))
-            print(len(heightSegConf))
-            print(len(heightSegType))
-            print(len(heightSegRefl))
-            print("Time")
-            print(len(time))
-            print(len(dateTime))
-
-            df_dict = {
-                "Latitude": latitude,
-                "Longitude": longitude,
-                "1FBH": data,
-                "2FBH": data2,
-                "3FBH": data3,
-                "2FBCONF": data2conf,
-                "2HSCONF": heightSegConf,
-                "2HSTYPE": heightSegType,
-                "2HSREFL": heightSegRefl,
-                "Quality": qualityFlag,
-                "GroundDist": distance,
-                "Time (seconds since 1/1/2018)": time,
-                "Calendar Time": dateTime
-            }
+            df_dict["Calendar Time"] = dateTime
 
             df = pd.DataFrame(dict([(k,pd.Series(v)) for k,v in df_dict.items()]))
             df = df.replace(r'^\s*$', np.nan, regex=True)
-            # df = df.dropna()
+
+            # Drop all rows where ICESAT has no FB height data across any of it's beams
+            df = df.dropna(subset=data_cols, thresh = 1)
 
             df.to_csv(OUT_FILE, date_format=None)
 
